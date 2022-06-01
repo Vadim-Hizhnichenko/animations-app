c845b985a35d75f260f8aa4d438142113e4e0188
Vadim Hizhnichenko <yankeevadim@gmail.com>
2022-06-01 19:32:31 +0300

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations:[
+    trigger('box', [
+      state('start', style({background: 'red'})),
+      state('end', style({background: 'yellow', transform: 'scale(1.3)'})),
+      state('special', style({
+        background: 'orange',
+        transform: 'scale(1.6)',
+        borderRadius: '50%'
+      })),
+      transition('start => end', animate(900)),
+      transition('end => start', animate(700)),
+      transition('special <=> *', [
+        style({background: 'blue'}), animate(800,
+          style({background: 'pink'})),
+          animate(800)
+      ]),
+      transition(':enter', [
+        style({ opacity : 0}),
+        animate(700)
+      ]),
+      transition(':leave', [
+        style({ opacity : 1}),
+        animate(700)
+      ])
+      
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'animations-app';
+  
+  boxState = 'start'
+  visible = true
+  animation(){
+    this.boxState = this.boxState === 'end' ? 'start' : 'end'
+  }
+
 }
